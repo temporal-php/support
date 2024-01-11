b388bb3907dfb506c44de3d0091793c6c165ca96
roxblnfk <roxblnfk@ya.ru>
2024-01-11 20:19:07 +0400

    Add VirtualPromise class

---
+++ src/VirtualPromise.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use React\Promise\PromiseInterface;
+
+/**
+ * @template T
+ * @yield T
+ * @extends PromiseInterface<T>
+ *
+ * Don't implement this interface and don't use it in a business logic, it is just for type hinting.
+ *
+ * Use the interface as a return type in activities to have type hints in workflows.
+ * Supported by Psalm and PHPStorm.
+ *
+ * Example:
+ *
+ * ```php
+ * #[ActivityInterface]
+ * class MyActivity {
+ *     /**
+ *      * @param non-empty-string $name
+ *      * @return VirtualPromise<non-empty-string>
+ *      *\/
+ *     #[ActivityMethod]
+ *     public function greet(string $name) {
+ *         return "Hello, {$name}!";
+ *     }
+ * }
+ * ```
+ */
+interface VirtualPromise extends PromiseInterface
+{
+}
